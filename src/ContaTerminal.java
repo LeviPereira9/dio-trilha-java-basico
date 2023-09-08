017e69791adae40236460de0ddcfc3f58a48c6e0
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2023-09-08 15:29:40 -0300

    Desafio

---
+++ src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.print("Por favor, informe o número de sua conta: ");
+            int contaNumero = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.print("Agora, informe o número da Agência: ");
+            String contaAgencia = scanner.nextLine();
+
+            System.out.print("Por favor, o nome completo: ");
+            String contaNome = scanner.nextLine();
+
+            double contaSaldo = 312.31;
+
+            System.out.printf(
+                    "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é a %s, conta Nº %d e seu saldo é de %.2f",
+                    contaNome, contaAgencia, contaNumero, contaSaldo);
+        }
+
+    }
+}
